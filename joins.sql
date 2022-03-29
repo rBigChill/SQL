7918c71938b7f78109bd607cee6e45c7a62188a4
Jorge Cisneros <cisneros.jorge.a@gmail.com>
2022-03-29 09:42:54 -0500

    started section 5 Joins

---
+++ joins.sql
@@ -0,0 +1,11 @@
+# How to combine tables
+
+# AS statement allows to create an alias for a column or result
+# executed and end of query
+SELECT column AS new_name
+FROM table
+
+SELECT SUM(column) AS new_name
+FROM table
+
+
